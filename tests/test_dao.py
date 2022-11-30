281212e3e6cb57001542b9e94afb4ca9ece447f6
Grigory Bukovsky <booqoffsky@yandex.ru>
2022-11-30 13:38:46 +0300

    Release/0.1.1

--- tests/test_dao.py
+++ tests/test_dao.py
@@ -1,11 +1,29 @@
-from typing import Optional
+import os
+from typing import Generator, Optional
 
 import pytest
-from neo4j import Driver
+from neo4j import Driver, GraphDatabase
+from yarl import URL
 
 from neo4j_python_migrations.dao import MigrationDAO
 from neo4j_python_migrations.migration import Migration, MigrationType
-from tests.conftest import username
+
+username = os.environ["NEO4J_MIGRATIONS_USER"]
+password = os.environ["NEO4J_MIGRATIONS_PASS"]
+host = os.environ["NEO4J_MIGRATIONS_HOST"]
+port = int(os.environ["NEO4J_MIGRATIONS_PORT"])
+scheme = os.environ["NEO4J_MIGRATIONS_SCHEME"]
+
+
+@pytest.fixture
+def neo4j_driver() -> Generator[Driver, None, None]:
+    with GraphDatabase.driver(
+        str(URL.build(scheme=scheme, host=host, port=port)),
+        auth=(username, password),
+    ) as driver:
+        yield driver
+        with driver.session() as session:
+            session.run("MATCH (n) DETACH DELETE n")
 
 
 def test_create_baseline(neo4j_driver: Driver) -> None:
