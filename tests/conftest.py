51958d6aa7fda54cb398d8e43b0d813a1feeffdf
Øyvind Evju <oyvind.evju@medisin.uio.no>
2024-05-23 14:37:11 +0200

    Skip DAO tests if neo4j is not available

---
+++ tests/conftest.py
@@ -0,0 +1,34 @@
+import os
+from typing import Generator
+from neo4j import Driver, GraphDatabase
+import pytest
+from yarl import URL
+
+
+username = os.environ.get("NEO4J_MIGRATIONS_USER")
+password = os.environ.get("NEO4J_MIGRATIONS_PASS")
+host = os.environ.get("NEO4J_MIGRATIONS_HOST", "localhost")
+port = int(os.environ.get("NEO4J_MIGRATIONS_PORT", 7687))
+scheme = os.environ.get("NEO4J_MIGRATIONS_SCHEME", "bolt")
+def can_connect_to_neo4j() -> bool:
+    try:
+        with GraphDatabase.driver(
+            str(URL.build(scheme=scheme, host=host, port=port)), auth=(username, password)
+        ) as driver:
+            driver.verify_connectivity()
+        return True
+    except Exception as e:
+        print(e)
+
+        return False
+
+
+@pytest.fixture
+def neo4j_driver() -> Generator[Driver, None, None]:
+    with GraphDatabase.driver(
+        str(URL.build(scheme=scheme, host=host, port=port)),
+        auth=(username, password),
+    ) as driver:
+        yield driver
+        with driver.session() as session:
+            session.run("MATCH (n) DETACH DELETE n")
--- tests/test_dao.py
+++ tests/test_dao.py
@@ -1,30 +1,14 @@
 import os
-from typing import Generator, Optional
+from typing import  Optional
 
 import pytest
-from neo4j import Driver, GraphDatabase
-from yarl import URL
+from neo4j import Driver
 
 from neo4j_python_migrations.dao import MigrationDAO
 from neo4j_python_migrations.migration import Migration, MigrationType
+from .conftest import can_connect_to_neo4j, username
 
-username = os.environ["NEO4J_MIGRATIONS_USER"]
-password = os.environ["NEO4J_MIGRATIONS_PASS"]
-host = os.environ["NEO4J_MIGRATIONS_HOST"]
-port = int(os.environ["NEO4J_MIGRATIONS_PORT"])
-scheme = os.environ["NEO4J_MIGRATIONS_SCHEME"]
-
-
-@pytest.fixture
-def neo4j_driver() -> Generator[Driver, None, None]:
-    with GraphDatabase.driver(
-        str(URL.build(scheme=scheme, host=host, port=port)),
-        auth=(username, password),
-    ) as driver:
-        yield driver
-        with driver.session() as session:
-            session.run("MATCH (n) DETACH DELETE n")
-
+pytestmark = pytest.mark.skipif(not can_connect_to_neo4j(), reason="Neo4j is not available")
 
 def test_create_baseline(neo4j_driver: Driver) -> None:
     dao = MigrationDAO(neo4j_driver)
